e548c39d359170fb26df994e6e594d9a8273f31d
RRIL97 <62291019+RRIL97@users.noreply.github.com>
2022-07-24 13:07:04 +0300

    Add files via upload

--- tutorial/shaders/cubemapShader.glsl
+++ tutorial/shaders/cubemapShader.glsl
@@ -12,6 +12,9 @@ uniform vec4 lightColor;
 uniform samplerCube sampler1;
 uniform vec4 lightDirection;
 
+uniform float sigma;
+uniform bool performBlurMotion;
+
 
 
 
@@ -21,6 +24,7 @@ float fogEffect( const float dist)
   float d = fogDensity * dist;
   return 1.0 - clamp(exp2(d * d * LOG2), 0.0, 1.0);
 }
+const float PI = 3.14159265;
 
 out vec4 Color;
 void main()
@@ -33,6 +37,40 @@ void main()
    Color = mix(tmpColor, fogColor, fogAmount);
   }
   else{
-    Color = texture(sampler1, lookat);
+  vec4  tmpColor = texture(sampler1, lookat);
+  if(performBlurMotion){
+  float blurSize = 5;
+  int horizontalPass = 0; 
+  vec3 texOffset = vec3(1.0, 1.0, 1.0);
+ 
+  vec2 p = texCoord0.xy;
+  float numBlurPixelsPerSide = float(blurSize / 2); 
+  
+  // Incremental Gaussian Coefficent Calculation (See GPU Gems 3 pp. 877 - 889)
+  vec3 incrementalGaussian;
+  incrementalGaussian.x = 1.0 / (sqrt(2.0 * PI) * sigma);
+  incrementalGaussian.y = exp(-0.5 / (sigma * sigma));
+  incrementalGaussian.z = incrementalGaussian.y * incrementalGaussian.y;
+
+  vec4 avgValue = vec4(0.0, 0.0, 0.0, 0.0);
+  float coefficientSum = 0.0;
+
+  // Take the central sample first...
+  avgValue +=tmpColor  * incrementalGaussian.x;
+  coefficientSum += incrementalGaussian.x;
+  incrementalGaussian.xy *= incrementalGaussian.yz;
+
+ for (float i = 1.0; i <= numBlurPixelsPerSide; i++) { 
+    avgValue +=  texture(sampler1, lookat - i * texOffset) * incrementalGaussian.x;   
+    avgValue += texture(sampler1, lookat + i * texOffset) * incrementalGaussian.x * incrementalGaussian.x;         
+    coefficientSum += 2.0 * incrementalGaussian.x;
+    incrementalGaussian.xy *= incrementalGaussian.yz;
+  }
+
+  Color = avgValue;
+  } else {
+   Color = tmpColor;
+  }
+
   }
 }  
